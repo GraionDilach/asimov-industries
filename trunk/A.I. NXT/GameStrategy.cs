3793a5215290ed295687ac5f654a3bbdf46a5005
vass.ildiko0316@gmail.com <vass.ildiko0316@gmail.com@90cfecba-e325-d909-8c2c-9f3339068ec3>
2013-03-24 18:35:41 +0000

    GameStrategie

--- trunk/A.I. NXT/GameStrategy.cs
+++ trunk/A.I. NXT/GameStrategy.cs
@@ -7,9 +7,90 @@ namespace A.I.NXT
 {
     class GameStrategy
     {
+        double xk = 112.9;//pálya közepe x koordináta szerint
+        double zk = 50.25;//pálya hossza z koordináta szerint
+        double ym = 80;//robot magassága a tetejétől számítva
+        double ykm = 40;//80 cm feletti rész
+        double atlo, kh, fordulatszög;
+        double menet = 2.5 * Math.PI; double rész;
+        double[] labdakoo;
+        double[,] adatok = new double[8, 4]; int k = 0; int i = 0; int j = 0;
+        int[] tömb = new int[4];
+        double[] különb = new double[4];
         public GameStrategy();
-        public void setCoordinates();
-        public void goToBasket();
+        public void setCoordinates()
+        {
+            atlo = (xk * xk) + (zk * zk);
+            kh = atlo + (107 * 107);
+            kh = Math.Sqrt(kh);
+            for (int j = 0; j < 4; j++)
+            {
+                adatok[i, j] = kh;
+            }
+            i++;
+            kezdoallapot();
+        }
+        public void goToBasket()
+        {
+            double x, y, z;
+            x = labdakoo[k];
+            k++;
+            y = labdakoo[k];
+            k++;
+            z = labdakoo[k];
+            k++;
+
+            adatok[i, 0] = M1(x, y, z);
+            adatok[i, 1] = M2(x, y, z);
+            adatok[i, 2] = M3(x, y, z);
+            adatok[i, 3] = M4(x, y, z);
+            i++;
+            különbség(i);
+        }
         public void goToBall();
+        public void különbség(int i)
+        {
+            int a = 2;
+            int b = 1;
+            if (i == 5)
+            {
+                a = 3; b = 2;
+            }
+            if (i == 6)
+            {
+                a = 4; b = 3;
+            }
+            if (i == 7)
+            {
+                a = 5; b = 4;
+            }
+            for (int j = 0; j < különb.Length; j++)
+            {
+                különb[j] = adatok[i - a, j] - adatok[i - b, j];
+            }
+            tömb = Menet(különb);
+        }
+        public int[] Tömb()
+        {
+            return tömb;
+        }
+        public double M1(double x, double y, double z)
+        {
+            x -= 9.6;
+            z -= 7.75;
+            atlo = (x * x) + (z * z);
+            kh = atlo + (ykm * ykm);
+            kh = Math.Sqrt(kh);
+            return kh;
+        }
+        public double M2(double x, double y, double z)
+        {
+            x += 9.6;
+            z -= 7.75;
+            atlo = (x * x) + (z * z);
+            kh = atlo + (ykm * ykm);
+            kh = Math.Sqrt(kh);
+            return kh;
+        }
     }
 }
