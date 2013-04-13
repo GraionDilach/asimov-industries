5e7631b5111c30b60e26d675e24b6f2ebf1368cd
graiondilach <graiondilach@gmail.com>
2013-04-13 17:22:02 +0000

    Thread-safe variant, added disconnect function written by Zoli first on Thursday but was crashy that time.
    First thread-safe code of mine, yay!

--- A.I. NXT/NxtControl.cs
+++ A.I. NXT/NxtControl.cs
@@ -12,10 +12,15 @@ namespace A.I.NXT
     /// It should maintain the syncs between the three
     /// devices, as in the 2 NXT bricks and the PC.
     /// </summary>
-    public class NxtControl 
+    public class NxtControl
     {
         bool NXTReady;  ///<boolean for NXT status, true, if ready, false, if busy
         bool NXT2Ready; ///<boolean for NXT2 status, true, if ready, false, if busy
+        bool NXTConnection;                ///<boolean for NXT connection, false if disconnected
+        bool NXT2Connection;                ///<boolean for NXT2 connection, false if disconnected
+
+        byte[] NXTMessage;      ///<buffer where the next message for the NXT to be sent will be placed
+        byte[] NXT2Message;     ///<buffer where the next message for the NXT2 to be sent will be placed
 
         Thread NXTListener;     ///<background thread for pinging and mailbox reading, NXT1
         Thread NXT2Listener;    ///<background thread for pinging and mailbox reading, NXT2
@@ -28,23 +33,27 @@ namespace A.I.NXT
         /// <param name="NXTport">COM port of the first NXT</param>
         /// <param name="NXT2port">COM port of the second NXT</param>
         public NxtControl(string NXTport, string NXT2port)
-        {     
-                //to-do: add handling for UI
-                BluetoothConnection.PortName = NXTport;
-                BluetoothConnection.Open();
-                NXTReady = true;
-                BluetoothConnection2.PortName = NXT2port;
-                BluetoothConnection2.Open();
-                NXT2Ready = true;
-                BluetoothConnection.DataReceived += GetMessageFromNXT;
-                BluetoothConnection2.DataReceived += GetMessageFromNXT2;
-                NXTListener = new Thread(ReadMailbox);
-                NXT2Listener = new Thread(ReadMailbox2);
-                NXTListener.Start();
-                NXT2Listener.Start();
+        {
+            //to-do: add handling for UI
+            BluetoothConnection.PortName = NXTport;
+            BluetoothConnection.Open();
+            NXTReady = true;
+            BluetoothConnection2.PortName = NXT2port;
+            BluetoothConnection2.Open();
+            NXT2Ready = true;
+            NXTConnection = true;
+            NXT2Connection = true;
+            NXTMessage = new byte[] { };
+            NXT2Message = new byte[] { };
+            BluetoothConnection.DataReceived += GetMessageFromNXT;
+            BluetoothConnection2.DataReceived += GetMessageFromNXT2;
+            NXTListener = new Thread(ReadMailbox);
+            NXT2Listener = new Thread(ReadMailbox2);
+            NXTListener.Start();
+            NXT2Listener.Start();
 
-          
-               
+
+
         }
 
         /// <summary>
@@ -54,16 +63,30 @@ namespace A.I.NXT
         void ReadMailbox()
         {
 
-            while (true)
+            while (NXTConnection)
             {
-                byte[] NXTMessage = { 0x00, 0x13, 0x0C, 0x00, 0x01 };
+                byte[] _NXTMessage;
                 Byte[] MessageLength = { 0x00, 0x00 };
+                lock (this)
+                {
+                    if (NXTMessage.Length != 0)
+                    {
+                        _NXTMessage = new byte[NXTMessage.Length];
+                        NXTMessage.CopyTo(_NXTMessage, 0);
+                        NXTMessage = new byte[] { };
+                    }
+                    else
+                    {
+                        _NXTMessage = new byte[] { 0x00, 0x13, 0x0B, 0x00, 0x01 };
+                    }
 
-                MessageLength[0] = (byte)NXTMessage.Length;
+                }
+
+                MessageLength[0] = (byte)_NXTMessage.Length;
                 BluetoothConnection.Write(MessageLength, 0, MessageLength.Length);
-                BluetoothConnection.Write(NXTMessage, 0, NXTMessage.Length);
+                BluetoothConnection.Write(_NXTMessage, 0, _NXTMessage.Length);
                 Thread.Sleep(75);
-               }
+            }
         }
 
         /// <summary>
@@ -72,14 +95,28 @@ namespace A.I.NXT
         /// </summary>
         void ReadMailbox2()
         {
-            while (true)
+            while (NXT2Connection)
             {
-                byte[] NXTMessage = { 0x00, 0x13, 0x0C, 0x00, 0x01 };
+                byte[] _NXTMessage;
                 Byte[] MessageLength = { 0x00, 0x00 };
+                lock (this)
+                {
+                    if (NXT2Message.Length != 0)
+                    {
+                        _NXTMessage = new byte[NXT2Message.Length];
+                        NXT2Message.CopyTo(_NXTMessage, 0);
+                        NXT2Message = new byte[] { };
+                    }
+                    else
+                    {
+                        _NXTMessage = new byte[] { 0x00, 0x13, 0x0B, 0x00, 0x01 };
+                    }
 
-                MessageLength[0] = (byte)NXTMessage.Length;
+                }
+
+                MessageLength[0] = (byte)_NXTMessage.Length;
                 BluetoothConnection2.Write(MessageLength, 0, MessageLength.Length);
-                BluetoothConnection2.Write(NXTMessage, 0, NXTMessage.Length);
+                BluetoothConnection2.Write(_NXTMessage, 0, _NXTMessage.Length);
                 Thread.Sleep(75);
             }
 
@@ -103,10 +140,14 @@ namespace A.I.NXT
                     {
                         //UI.NXTTextbox = "Executing command..."
                     }
+
                     if (CurrByte == 0xFA)
                     {
-                        //UI.NXTTextbox = "Ready."
-                        NXTReady = true;
+                        lock (this)
+                        {
+                            //UI.NXTTextbox = "Ready."
+                            NXTReady = true;
+                        }
                     }
 
                 }
@@ -128,18 +169,21 @@ namespace A.I.NXT
                 byte CurrByte = (byte)BluetoothConnection2.ReadByte();
                 if (i == 5)
                 {
-                   if(CurrByte == 0xFF)
-                   {
-                      //UI.NXTTextbox2 = "Executing command..."
-                   }
-                   if (CurrByte == 0xFA)
-                   {
-                       //UI.NXTTextbox2 = "Ready."
-                       NXT2Ready = true;
-                   }
- 
+                    if (CurrByte == 0xFF)
+                    {
+                        //UI.NXTTextbox2 = "Executing command..."
+                    }
+                    if (CurrByte == 0xFA)
+                    {
+                        lock (this)
+                        {
+                            //UI.NXT2Textbox = "Ready."
+                            NXT2Ready = true;
+                        }
+                    }
+
                 }
-            } 
+            }
             BluetoothConnection2.DiscardInBuffer();
 
         }
@@ -153,13 +197,13 @@ namespace A.I.NXT
         /// <param name="m4">Amount of turns Motor 4 shall do (in degree)</param>
         public void StartOperation(int m1, int m2, int m3, int m4)
         {
-            bool negative= false;
+            bool negative = false;
             int residuary;
             byte[] NXTMsg = new byte[17];
             byte[] NXT2Msg = new byte[17];
             int i;
             // m2 motor értékeinek feldolgozása
-            
+
             for (i = 16; i > 11; i--)
             {
                 NXTMsg[i] = (byte)0x30;
@@ -168,12 +212,12 @@ namespace A.I.NXT
             i--;
             if (m2 < 0)
             {
-                negative=true;
-                m2=m2*-1;
+                negative = true;
+                m2 = m2 * -1;
             }
             {
-            for (; i > 5; i--)
-            {   
+                for (; i > 5; i--)
+                {
                     if (m2 > 0)
                     {
                         residuary = m2 % 10;
@@ -181,11 +225,10 @@ namespace A.I.NXT
 
                     }
                     else
-                                 
-                        {
-                            NXTMsg[i] = (byte)0x30;
-                        }
-                    
+                    {
+                        NXTMsg[i] = (byte)0x30;
+                    }
+
                     m2 = m2 / 10;
                 }
             }
@@ -214,7 +257,7 @@ namespace A.I.NXT
                 {
 
                     NXTMsg[i] = (byte)0x30;
-                    
+
                 }
                 m1 = m1 / 10;
             }
@@ -263,13 +306,15 @@ namespace A.I.NXT
                 NXT2Msg[6] = (byte)'-';
                 negative = false;
             }
-         // az m3 motor feldolgozása
+            NXTMsg[i] = (byte)0x30;
+            i--;
+            // az m3 motor feldolgozása
             if (m3 < 0)
             {
                 negative = true;
                 m3 = m3 * -1;
             }
-            for (; i >=0; i--)
+            for (; i >= 0; i--)
             {
                 if (m3 > 0)
                 {
@@ -329,30 +374,25 @@ namespace A.I.NXT
             NXT2Ready = false;
             NXTReady = false;
 
-            
-            byte datalength = Convert.ToByte(NXT1Msg.Length + 1);
-            byte[] NxtHeader = { 0x00, 0x09, 0x00, datalength };
-            byte[] NXT1Message = new byte[NxtHeader.Length + NXT1Msg.Length + 1];
-            NxtHeader.CopyTo(NXT1Message, 0);
-            NXT1Msg.CopyTo(NXT1Message, NxtHeader.Length);
+            lock (this)
+            {
 
-            Byte[] NXT1MsgLen = { 0x00, 0x00 };
-            NXT1MsgLen[0] = (byte)NXT1Message.Length;
+                byte datalength = Convert.ToByte(NXT1Msg.Length + 1);
+                byte[] NxtHeader = { 0x00, 0x09, 0x00, datalength };
+                NXTMessage = new byte[NxtHeader.Length + NXT1Msg.Length + 1];
+                NxtHeader.CopyTo(NXTMessage, 0);
+                NXT1Msg.CopyTo(NXTMessage, NxtHeader.Length);
 
-            byte data2length = Convert.ToByte(NXT2Msg.Length + 1);
-            byte[] Nxt2Header = { 0x00, 0x09, 0x00, data2length };
-            byte[] NXT2Message = new byte[Nxt2Header.Length + NXT2Msg.Length + 1];
-            Nxt2Header.CopyTo(NXT2Message, 0);
-            NXT2Msg.CopyTo(NXT2Message, Nxt2Header.Length);
+                Byte[] NXT1MsgLen = { 0x00, 0x00 };
+                NXT1MsgLen[0] = (byte)NXTMessage.Length;
 
-            Byte[] NXT2MsgLen = { 0x00, 0x00 };
-            NXT2MsgLen[0] = (byte)NXT2Message.Length;
+                byte data2length = Convert.ToByte(NXT2Msg.Length + 1);
+                byte[] Nxt2Header = { 0x00, 0x09, 0x00, data2length };
 
-            BluetoothConnection.Write(NXT1MsgLen, 0, NXT1MsgLen.Length);
-            BluetoothConnection.Write(NXT1Message, 0, NXT1Message.Length);
-
-            BluetoothConnection2.Write(NXT2MsgLen, 0, NXT2MsgLen.Length);
-            BluetoothConnection2.Write(NXT2Message, 0, NXT2Message.Length);
+                NXT2Message = new byte[Nxt2Header.Length + NXT2Msg.Length + 1];
+                Nxt2Header.CopyTo(NXT2Message, 0);
+                NXT2Msg.CopyTo(NXT2Message, Nxt2Header.Length);
+            }
 
             while ((NXTReady == false) && (NXT2Ready == false))
             {
@@ -361,8 +401,22 @@ namespace A.I.NXT
             }
         }
 
+        /// <summary>
+        /// This function closes down the Bluetooth connections of the NXTs.
+        /// </summary>
+        public void DisConnect()
+        {
+            lock (this)
+            {
+                NXTConnection = false;
+                NXT2Connection = false;
+            }
+            BluetoothConnection.DataReceived += null;
+            BluetoothConnection2.DataReceived += null;
+            BluetoothConnection.Close();
+            BluetoothConnection2.Close();
+        }
 
-        
     }
 
 }
